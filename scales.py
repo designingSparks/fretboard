efed02b59c074dede9b1e386fed22b13e9a3ca14
John <surfer10@gmail.com>
2025-10-30 19:50:17 +0100

    Added Cmaj pos5 scale

--- scales.py
+++ scales.py
@@ -48,3 +48,30 @@ C_MAJOR_POS4_PLAY = [
     [('G', 4), TON],
     [('G', 5), TON],
 ]
+
+C_MAJOR_POS5_HIGHLIGHT = [
+    ('E', 5), ('E', 7), ('E', 8), # Low E string
+    ('A', 5),('A', 7),('A', 8),
+    ('D', 5),('D', 7),('D', 9),
+    ('G', 5),('G', 7),
+    ('B', 5),('B', 6),('B', 8),
+    ('e', 5),('e', 7),('e', 8) # High e string
+]
+
+#This is the actual sequence that is played: C to C, i.e. a subset of the scale
+C_MAJOR_POS5_PLAY = [
+    #E string
+    [('E', 8), TON],
+    #A string
+    [('A', 5), TON],
+    [('A', 7), TON],
+    [('A', 8), TON],
+    # D string
+    [('D', 5), TON],
+    [('D', 7), TON],
+    [('D', 9), TON],
+    # G string
+    [('G', 5), TON],
+    # [('G', 4), TON],
+    # [('G', 5), TON],
+]
